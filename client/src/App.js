98ca2164d833c8faa703af1ebff14e8ff4b50cb4
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-08-19 00:17:32 -0400

    Feat: add basic app grid layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Header, Footer } from "./components/container";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div>
-      <div>
+    <div className={styles.appContainer}>
+      <div className={styles.header}>
         <Header />
       </div>
-      <div>
+      <div className={styles.main}>
         <Outlet />
       </div>
-      <div>
+      <div className={styles.footer}>
         <Footer />
       </div>
     </div>
