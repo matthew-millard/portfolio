0c5137ee824e398521b34981d7f228a3869f4f03
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-08-25 14:27:35 -0400

    Feat(contact): Enhance contact form component and styling
    - Update Contact component with improved input handling
    - Standardize input and textarea styles for cross-browser consistency
    - Revise introductory message to encourage user engagement

--- client/src/components/container/Footer/Footer.jsx
+++ client/src/components/container/Footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./Footer.module.css";
 
 function Footer() {
   return (
-    <footer>
+    <footer className={styles.footerContainer}>
       <h1>Footer</h1>
     </footer>
   );
--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -1,9 +1,151 @@
-import React from "react";
+import React, { useState } from "react";
+import isValidEmail from "../../utils/isValidEmail";
+import styles from "./Contact.module.css";
+import utilStyles from "../../styles/utilities.module.css";
 
 function Contact() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [message, setMessage] = useState("");
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+  const [emailAddressError, setEmailAddressError] = useState("");
+  const [messageError, setMessageError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+
+  const handleChange = ({ target: { id, value } }) => {
+    switch (id) {
+      case "firstName":
+        setFirstName(value);
+        break;
+      case "lastName":
+        setLastName(value);
+        break;
+      case "email":
+        setEmailAddress(value);
+        break;
+      case "message":
+        setMessage(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleBlur = ({ target: { id, value } }) => {
+    if (!value) {
+      switch (id) {
+        case "firstName":
+          setFirstNameError("This field is required");
+          break;
+        case "lastName":
+          setLastNameError("This field is required");
+          break;
+        case "email":
+          setEmailAddressError("This field is required");
+          break;
+        case "message":
+          setMessageError("This field is required");
+          break;
+        default:
+          break;
+      }
+    } else {
+      switch (id) {
+        case "firstName":
+          setFirstNameError("");
+          break;
+        case "lastName":
+          setLastNameError("");
+          break;
+        case "email":
+          setEmailAddressError(isValidEmail(value) ? "" : "Invalid email address");
+          break;
+        case "message":
+          setMessageError("");
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className={styles.contactContainer}>
+      <h1 className={styles.heading}>Contact</h1>
+      <p className={styles.subHeading}>
+        I'm always eager to discuss web projects or chat about tech! Don't hesitate to reach out via
+        email or connect with me on social media.
+      </p>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.formFieldContainer}>
+          <div className={styles.inputGroup}>
+            <label htmlFor="firstName">First name</label>
+            <input
+              type="text"
+              id="firstName"
+              value={firstName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              required
+            />
+            <p className={firstNameError ? utilStyles.error : utilStyles.errorHidden}>
+              {firstNameError}
+            </p>
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="lastName">Last name</label>
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              required
+            />
+            <p className={lastNameError ? utilStyles.error : utilStyles.errorHidden}>
+              {lastNameError}
+            </p>
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={emailAddress}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              required
+            />
+            <p className={emailAddressError ? utilStyles.error : utilStyles.errorHidden}>
+              {emailAddressError}
+            </p>
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              rows="5"
+              value={message}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              required
+            />
+            <p className={messageError ? utilStyles.error : utilStyles.errorHidden}>
+              {messageError}
+            </p>
+          </div>
+        </div>
+        {/* TODO: Create Button component */}
+        <div>
+          <button type="submit">Send</button>
+        </div>
+      </form>
     </div>
   );
 }
